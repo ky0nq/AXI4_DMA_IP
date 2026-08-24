1a0909360363f758f0197207ce40b33be0031542
ky0nq <141110673+ky0nq@users.noreply.github.com>
2026-08-24 17:29:57 +0900

    Update read_datapath.v
    Need to more update..

--- DMA_CNTL/Read_Engine/read_datapath.v
+++ DMA_CNTL/Read_Engine/read_datapath.v
@@ -6,57 +6,77 @@ module read_datapath #(
     parameter LEN_WIDTH   = 32,
     parameter BURST_WIDTH = 8
 )(
-    input                             clk,
-    input                             rst_n,
+    input                           clk,
+    input                           rst_n,
 
     // Control Unit -> Datapath
-    input                             en,             // state == S_DATA
-    input                             init,           // IDLE -> DATA transition = 1 pulse
-    input                             abort,          // Force Shutdown
+    input                           en,             // state == S_DATA
+    input                           init,           // IDLE -> DATA transition = 1 pulse
+    input                           abort,          // Force Shutdown
 
     // Register Map
-    input      [ADDR_WIDTH-1:0]       src_addr,
-    input      [LEN_WIDTH-1:0]        length,         // total transfer burst byte
-    input      [BURST_WIDTH-1:0]      burst_cfg,      // AXI ARLEN Maximum value (in Register Setting value)
+    input       [ADDR_WIDTH-1:0]    src_addr,
+    input       [LEN_WIDTH-1:0]     length,         // total transfer burst byte
+    input       [BURST_WIDTH+1:0] burst_cfg,        // AXI ARLEN Maximum value (in Register Setting value) [7:0] & burst type [9:8]
 
     // Datapath -> Control Unit
-    output                            r_hs,
-    output                            xfer_done,      // transfer done or abort assertion
-    output     [ADDR_WIDTH-1:0]       err_addr,
+    output                          r_hs,
+    output                          xfer_done,      // transfer done or abort assertion
+    output      [ADDR_WIDTH-1:0]    err_addr,
 
     // FIFO
-    output reg                        fifo_wr_en,
-    output reg [DATA_WIDTH-1:0]       fifo_wr_data,
-    input                             fifo_full,
+    output reg                      fifo_wr_en,
+    output reg  [DATA_WIDTH-1:0]    fifo_wr_data,
+    input                           fifo_full,
 
-    // AXI4 AR/R channel
-    output reg [3:0]                  arid,
-    output reg [ADDR_WIDTH-1:0]       araddr,
-    output reg [BURST_WIDTH-1:0]      arlen,
-    output     [2:0]                  arsize,
-    output     [1:0]                  arburst,
-    output reg                        arvalid,
-    input                             arready,
-    input      [DATA_WIDTH-1:0]       rdata,
-    input                             rvalid,
-    input                             rlast,
-    input      [3:0]                  rid,
-    output                            rready
+    // AXI4 AR / R channel
+    output reg  [3:0]               arid,
+    output reg  [ADDR_WIDTH-1:0]    araddr,
+    output reg  [BURST_WIDTH-1:0]   arlen,
+    output      [2:0]               arsize,
+    output      [1:0]               arburst,
+    output reg                      arvalid,
+    input                           arready,
+    input       [DATA_WIDTH-1:0]    rdata,
+    input                           rvalid,
+    input                           rlast,
+    input       [3:0]               rid,
+    output                          rready
 );
 
-    localparam MASTER_ID      = 1'b1;                     // ID[3] : DMA = 1
-    localparam BYTES_PER_BEAT = DATA_WIDTH/8;             // 1 beat (byte)
-    localparam ADDR_LSB       = $clog2(BYTES_PER_BEAT);   // byte offset
-    localparam PAGE_BYTES     = 4096;                     // AXI4-Full Maximum burst size = 4KB boundary
-    localparam PAGE_LSB       = $clog2(PAGE_BYTES);       // 12
+    localparam MASTER_ID      = 1'b1;                       // ID[3] : DMA = 1
+    localparam BYTES_PER_BEAT = DATA_WIDTH/8;               // 1 beat (byte)  // ex) 32-byte/8 = 4-byte
+    localparam ADDR_LSB       = $clog2(BYTES_PER_BEAT);     // byte offset    // 2
+    localparam PAGE_BYTES     = 4096;                       // AXI4-Full Maximum burst size = 4KB boundary
+    localparam PAGE_LSB       = $clog2(PAGE_BYTES);         // 12
 
-    assign arsize  = 3'b010;
-    assign arburst = 2'b01;
+    localparam [2:0] ARSIZE_VAL = ADDR_LSB;                 
 
+    assign arsize  = ARSIZE_VAL;           
+    /* 
+    3'b000 Beat size = 1-Byte
+    3'b001 Beat size = 2-Byte
+    3'b010 Beat size = 4-Byte
 
-    reg  [ADDR_WIDTH-1:0] cur_addr;        // Next AR address
-    reg  [LEN_WIDTH-1:0]  req_byte_cnt;    // request byte
-    reg  [LEN_WIDTH-1:0]  total_byte_cnt;  // total received byte
+    Bytes per Beat = 2^ARSIZE
+    */
+    wire [1:0]             burst_type_cfg;
+    wire [BURST_WIDTH-1:0] burst_len_cfg;
+
+    assign burst_type_cfg = burst_cfg[BURST_WIDTH+1:BURST_WIDTH];
+    assign burst_len_cfg  = burst_cfg[BURST_WIDTH-1:0];
+
+    assign arburst = burst_type_cfg;
+
+    /*
+    burst_cfg[9:8] = 00 → ARBURST = FIXED
+    burst_cfg[9:8] = 01 → ARBURST = INCR
+    burst_cfg[9:8] = 10 → ARBURST = WRAP
+    */
+
+    reg  [ADDR_WIDTH-1:0] cur_addr;         // Next AR address
+    reg  [LEN_WIDTH-1:0]  req_byte_cnt;     // request byte
+    reg  [LEN_WIDTH-1:0]  total_byte_cnt;   // total received byte
 
     reg  [1:0]            slot_busy;
     reg  [ADDR_WIDTH-1:0] slot_addr [0:1];
@@ -71,13 +91,13 @@ module read_datapath #(
     assign rready = !fifo_full;
 
     wire [1:0] slave_sel;
-    assign slave_sel = cur_addr[14] ? 2'b01 : 2'b00;    // ROM:00 / RAM:01
+    assign slave_sel = cur_addr[ADDR_WIDTH-1] ? 2'b01 : 2'b00;  // ROM:00 / RAM:01
 
     wire req_pending;
-    assign req_pending = (req_byte_cnt < length);
+    assign req_pending = (req_byte_cnt < length);   // Remain request
 
-    reg  [1:0] outstanding_cnt;
-    reg  [1:0] active_slave;
+    reg [1:0] outstanding_cnt;
+    reg [1:0] active_slave;
 
     always @(posedge clk) begin
         if (!rst_n) begin
@@ -134,55 +154,111 @@ module read_datapath #(
     assign outstanding_ok = (outstanding_cnt < 2'd2);
 
     // new AR possible condition check
-    wire ar_can_issue;
     // -- Guard --
     // !init : init cycle = Before cur_addr/req_byte_cnt update
-    assign ar_can_issue = en && !init && !arvalid && req_pending && !abort_lat &&
-                          outstanding_ok && same_slave_ok;
+    wire ar_can_issue;
+    assign ar_can_issue = en && !init && !arvalid && req_pending && !abort_lat && outstanding_ok && same_slave_ok;
 
     // AR channel Done = Not exists Request Or Abort assertion
-    wire ar_done;
-    assign ar_done = !req_pending || abort_lat;
-    // Transfer Done
-    assign xfer_done = !init && ar_done && (outstanding_cnt == 2'd0);
+    wire ar_done;   // AR request X
+    assign ar_done = !req_pending || abort_lat; 
+
+    // Transfer Done == Burst Response Done
+    assign xfer_done = !init && ar_done && (outstanding_cnt == 2'd0);   // Do exists request != Transfer Done
+
 
     //-----------------------------------------------------------
-    //  (1) desired_beats     : burst_cfg
-    //  (2) beats_to_boundary : cur_addr ~ 4KB boundary addr
-    //                          -> non-alignment burst -> burst length automatic setting (shortest)
-    //                             && 4KB boundary check
-    //  (3) remain_beats      : length - req_byte_cnt = #remained beat
-    //                          -> last burst length < beat => burst length automatic setting (shortest)
+    //  (1) desired_beats      : burst_cfg
+    //  (2) beats_to_boundary  : cur_addr ~ 4KB boundary addr
+    //                           -> non-alignment burst -> burst length automatic setting (shortest)
+    //                              && 4KB boundary check
+    //  (3) remain_beats       : length - req_byte_cnt = #remained beat
+    //                           -> last burst length < beat => burst length automatic setting (shortest)
     //-----------------------------------------------------------
     wire [LEN_WIDTH-1:0]   remain_bytes;
     wire [LEN_WIDTH-1:0]   remain_beats;
     wire [LEN_WIDTH-1:0]   bytes_to_boundary;
     wire [LEN_WIDTH-1:0]   beats_to_boundary;
     wire [LEN_WIDTH-1:0]   desired_beats;
-    wire [LEN_WIDTH-1:0]   safe_beats;
-    // Current burst's #beat (req_pending=1, always 1~256 range)
-    // req_pending is request remaining state
-    wire [LEN_WIDTH-1:0]   safe_bytes;   // safe_beats -> #bytes (beat converts byte)
-    wire [BURST_WIDTH-1:0] safe_arlen;   // AXI ARLEN = #beat - 1 (ARLEN = 0 -> 1 beat)
+//    wire [LEN_WIDTH-1:0]   safe_beats;      // Current burst's #beat (req_pending=1, always 1~256 range)
+//    wire [LEN_WIDTH-1:0]   safe_bytes;      // safe_beats -> #bytes (beat converts byte)
+//    wire [BURST_WIDTH-1:0] safe_arlen;      // AXI ARLEN = #beat - 1 (ARLEN = 0 -> 1 beat)
+
+    wire [LEN_WIDTH-1:0]   safe_beats_incr;
+    wire [LEN_WIDTH-1:0]   safe_beats_fixed;
+    wire [LEN_WIDTH-1:0]   safe_beats_wrap;
+    wire [LEN_WIDTH-1:0]   safe_beats;        // Checking Burst Size  
+
+    wire [LEN_WIDTH-1:0]   safe_bytes;        // Calculate Next Busrt address 
+    wire [BURST_WIDTH-1:0] safe_arlen;
 
     // calculate remain
-    assign remain_bytes      = length - req_byte_cnt;
-    assign remain_beats      = remain_bytes >> ADDR_LSB;
+    assign remain_bytes      = length - req_byte_cnt;          
+    assign remain_beats      = remain_bytes >> ADDR_LSB;    // Sending Beats per 4-Byte (1 Beat = 4-Byte)
 
     assign bytes_to_boundary = PAGE_BYTES -
-                               {{(LEN_WIDTH-PAGE_LSB){1'b0}}, cur_addr[PAGE_LSB-1:0]};
-    assign beats_to_boundary = bytes_to_boundary >> ADDR_LSB;
+                               {{(LEN_WIDTH-PAGE_LSB){1'b0}}, cur_addr[PAGE_LSB-1:0]}; // == bytes_to_boundary = 4096 - (cur_addr % 4096)
+    assign beats_to_boundary = bytes_to_boundary >> ADDR_LSB;                          // transition bytes to beats
 
-    assign desired_beats     = {{(LEN_WIDTH-BURST_WIDTH){1'b0}}, burst_cfg} + 1'b1;
+    assign desired_beats     = {{(LEN_WIDTH-BURST_WIDTH){1'b0}}, burst_len_cfg} + 1'b1;     // Beats = ARLEN + 1
 
-    assign safe_beats = (desired_beats < beats_to_boundary) ?
-                             ((desired_beats < remain_beats) ? desired_beats : remain_beats) :
-                             ((beats_to_boundary < remain_beats) ? beats_to_boundary : remain_beats);
+    /* 
+    ARLEN = 0 , Beat = 1
+    ARLEN = 1 , Beat = 2
+    ARLEN = 3 , Beat = 4
+    */
 
+    // ---------------------------------------------------------
+    // FIXED Burst
+    // AXI4 FIXED burst supports up to 16 beats.
+    // ---------------------------------------------------------
+    assign safe_beats_fixed =
+        (remain_beats < desired_beats) ?
+            ((remain_beats < 16) ? remain_beats : 16) :
+            ((desired_beats < 16) ? desired_beats : 16); // FIXED: min(Max Beats, Remaining Beats, 16)
+
+
+    // ---------------------------------------------------------
+    // INCR Burst
+    // Must not cross 4KB boundary.
+    // ---------------------------------------------------------
+    assign safe_beats_incr =
+        (desired_beats < beats_to_boundary) ?
+            ((desired_beats < remain_beats) ?
+                desired_beats : remain_beats) :
+            ((beats_to_boundary < remain_beats) ?
+                beats_to_boundary : remain_beats); // INCR: min(Max Beats, Beats to 4KB Boundary, Remaining Beats)
+
+
+    // ---------------------------------------------------------
+    // WRAP Burst
+    // AXI4 WRAP supports only 2 / 4 / 8 / 16 beats.
+    // Choose the largest legal size that does not exceed
+    // desired_beats and remain_beats.
+    // ---------------------------------------------------------
+    assign safe_beats_wrap =
+        ((desired_beats >= 16) && (remain_beats >= 16)) ? 16 :
+        ((desired_beats >=  8) && (remain_beats >=  8)) ?  8 :
+        ((desired_beats >=  4) && (remain_beats >=  4)) ?  4 :
+        ((desired_beats >=  2) && (remain_beats >=  2)) ?  2 :
+                                                        0;
+
+
+    // ---------------------------------------------------------
+    // Select Burst calculation
+    // ---------------------------------------------------------
+    assign safe_beats =
+        (burst_type_cfg == 2'b00) ? safe_beats_fixed :
+        (burst_type_cfg == 2'b01) ? safe_beats_incr  :
+        (burst_type_cfg == 2'b10) ? safe_beats_wrap  :
+                                    0;
+
+    // ======================= Using AXI Operation =========================
+    // Convert the selected Burst Beat count to Byte count
     assign safe_bytes = safe_beats << ADDR_LSB;
-    // desired_beats <= 256 -> safe_beats <=256 -> 8bit truncate == safe
-    // 32-bit value -> Use only LSB 8-bit
+    // Convert the selected Burst Beat count to AXI ARLEN (ARLEN = Beats - 1)
     assign safe_arlen = safe_beats[BURST_WIDTH-1:0] - 1'b1;
+    // ======================================================================
 
     // Register in Datapath Update
     always @(posedge clk) begin
@@ -191,16 +267,30 @@ module read_datapath #(
             req_byte_cnt   <= {LEN_WIDTH{1'b0}};
             total_byte_cnt <= {LEN_WIDTH{1'b0}};
         end
-        else if (init) begin    // initialize
+        else if (init) begin    // initialize - DMA Read Transfer Start 
             cur_addr       <= src_addr;
             req_byte_cnt   <= {LEN_WIDTH{1'b0}};
             total_byte_cnt <= {LEN_WIDTH{1'b0}};
         end
         else begin
             if (r_hs)           // R channel handshake
-                total_byte_cnt <= total_byte_cnt + (DATA_WIDTH/8);
+                total_byte_cnt <= total_byte_cnt + BYTES_PER_BEAT;
             if (ar_hs) begin    // AR channel handshake
-                cur_addr     <= cur_addr     + safe_bytes[ADDR_WIDTH-1:0];
+                case (burst_type_cfg)
+                    2'b00: begin // FIXED
+                        cur_addr <= cur_addr;
+                    end
+                    2'b01: begin // INCR
+                        cur_addr <= cur_addr + safe_bytes[ADDR_WIDTH-1:0];
+                    end
+                    2'b10: begin // WRAP
+                        // WRAP address calculation 별도 구현 필요
+                    end
+                    default: begin
+                        cur_addr <= cur_addr;
+                    end
+                endcase
+
                 req_byte_cnt <= req_byte_cnt + safe_bytes;
                 slot_addr[next_num] <= cur_addr;
             end
@@ -227,15 +317,9 @@ module read_datapath #(
     end
 
     // R channel
-    always @(posedge clk) begin
-        if (!rst_n) begin
-            fifo_wr_en   <= 1'b0;
-            fifo_wr_data <= {DATA_WIDTH{1'b0}};
-        end
-        else begin
-            fifo_wr_en   <= r_hs;
-            fifo_wr_data <= rdata;  // FIFO write == Read Operation
-        end
+    always @(*) begin
+        fifo_wr_en   = r_hs;
+        fifo_wr_data = rdata;  // FIFO write == Read Operation
     end
 
 endmodule
